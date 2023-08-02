056c1d215934c89b60e69292730827f31f2c52ae
Asten <>
2023-08-02 16:33:25 +0800

    Add Starknet command and handle StarknetBlockNumber and StarknetCall commands

--- madara_cli/src/main.rs
+++ madara_cli/src/main.rs
@@ -3,7 +3,7 @@ use serde_json::{json, Value};
 use std::{collections::HashMap, vec};
 use structopt::StructOpt;
 
-use madara_cli_core::Opt;
+use madara_cli_core::{Command, Opt};
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
@@ -11,27 +11,68 @@ async fn main() -> Result<(), Error> {
 
     let rpc_url = opt.rpc_url;
 
-    let payload = json!({
-        "jsonrpc": "2.0",
-        "method": opt.method,
-        "params": serde_json::from_str::<Vec<String>>(&opt.params.unwrap_or("[]".to_string())).unwrap_or(vec![]),
-        "id": "1",
-    });
+    match opt.command {
+        Command::StarknetBlockNumber => {
+            let payload = json!({
+                "jsonrpc": "2.0",
+                "method": "starknet_blockNumber",
+                "params": [],
+                "id": "1",
+            });
 
-    let client = reqwest::Client::new();
-    let response: HashMap<String, Value> = client
-        .post(rpc_url)
-        .header("Content-Type", "application/json")
-        .json(&payload)
-        .send()
-        .await?
-        .json()
-        .await?;
+            let client = reqwest::Client::new();
+            let response: HashMap<String, Value> = client
+                .post(rpc_url)
+                .header("Content-Type", "application/json")
+                .json(&payload)
+                .send()
+                .await?
+                .json()
+                .await?;
 
-    if response.contains_key("error") {
-        println!("Error: {:?}", response["error"]);
-    } else {
-        println!("Response: {:?}", response["result"]);
+            if response.contains_key("error") {
+                println!("Error: {:?}", response["error"]);
+            } else {
+                println!("Response: {:?}", response["result"]);
+            }
+        }
+        Command::StarknetCall {
+            contract_address,
+            entry_point_selector,
+            calldata,
+            block_reference,
+        } => {
+            println!("contract_address: {:?}", contract_address);
+
+            let payload = json!({
+                "jsonrpc": "2.0",
+                "method": "starknet_call",
+                "params": [{
+                    "contract_address": contract_address,
+                    "entry_point_selector": entry_point_selector,
+                    "calldata": calldata,
+                    },
+                    block_reference
+                ],
+                "id": "1",
+            });
+
+            let client = reqwest::Client::new();
+            let response: HashMap<String, Value> = client
+                .post(rpc_url)
+                .header("Content-Type", "application/json")
+                .json(&payload)
+                .send()
+                .await?
+                .json()
+                .await?;
+
+            if response.contains_key("error") {
+                println!("Error: {:?}", response["error"]);
+            } else {
+                println!("Response: {:?}", response["result"]);
+            }
+        }
     }
 
     Ok(())
--- madara_cli_core/src/lib.rs
+++ madara_cli_core/src/lib.rs
@@ -1,14 +1,28 @@
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
+pub enum Command {
+    #[structopt(name = "get-blocknumber")]
+    StarknetBlockNumber,
+    #[structopt(name = "call-contract")]
+    StarknetCall {
+        #[structopt(long)]
+        contract_address: String,
+        #[structopt(long)]
+        entry_point_selector: String,
+        #[structopt(long)]
+        calldata: Vec<String>,
+        #[structopt(default_value = "latest")]
+        block_reference: String,
+    },
+}
+
+#[derive(Debug, StructOpt)]
 #[structopt(name = "madara_cli", about = "CLI for Madara node.")]
 pub struct Opt {
     #[structopt(long, default_value = "http://0.0.0.0:9944")]
     pub rpc_url: String,
 
-    #[structopt(short = "m", long = "method")]
-    pub method: String,
-
-    #[structopt(short = "p", long = "params")]
-    pub params: Option<String>,
+    #[structopt(subcommand)]
+    pub command: Command,
 }
